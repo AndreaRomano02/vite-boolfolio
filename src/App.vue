70a1a6c21d920109bdddece4cc71fe30a437de3b
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-11 17:40:48 +0200

    Add Project Card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,58 @@
 <script>
+import ProjectCard from './components/ProjectCard.vue';
+import axios from 'axios';
+export default {
+  components: { ProjectCard },
+  data() {
+    return {
+      projects: [],
+      projectLinks: [],
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(res => {
+        this.projects = res.data.projects.data;
+        this.projectLinks = res.data.projects.links;
+        console.log(this.projects)
+      })
+    }
+  },
+  created() {
+    this.fetchProjects()
+  }
+}
 </script>
-
+ 
 <template>
-  <h1 class="text-danger">TEST</h1>
-</template>
+  <main class="container">
+    <h1>Projects</h1>
+    <div class="row row-cols-3 gap-3">
+    </div>
 
-<style scoped>
-</style>
+    <table class="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Titolo</th>
+          <th scope="col">Tipo</th>
+          <th scope="col">Linguaggi</th>
+          <th scope="col">Descrizione</th>
+          <th scope="col">Link</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProjectCard v-if="projects.length" class="col" v-for="project in projects" :key="project.id"
+          :project="project" />
+        <tr v-else>
+          <td colspan="7" class="text-center">
+            <h1>NON CI SONO PROGETTI</h1>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
+</template>
+ 
+<style scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    props: {
+        project: Object,
+    }
+}
+</script>
+ 
+<template>
+    <tr>
+        <!--  * ID -->
+        <th class="align-middle">{{ project.id }}</th>
+
+        <!--  * Titolo -->
+        <th class="align-middle">{{ project.title }}</th>
+
+        <!--  * Tipo -->
+        <th class="align-middle text-center">
+            <span v-if="project.type" class="badge rounded-pill" :style="{ 'background-color': project.type.color }">{{
+                project.type.label }}</span>
+
+            <span v-else>-</span>
+        </th>
+
+        <!--  * Linguaggio -->
+        <th class="align-middle text-center py-5">
+            <span v-if="project.technologies" v-for="   technology    in    project.technologies   " :key="technology.id"
+                class="badge rounded-pill me-2 mb-2" :style="{ 'background-color': technology.color }">{{
+                    technology.label }}</span>
+
+            <span v-else>-</span>
+        </th>
+
+        <!--  * Descrizione -->
+        <td class="align-middle">{{ project.description.substr(0, 50) + '...' }}</td>
+
+        <!--  * Link GitHub -->
+        <td class="align-middle"><a href="{{ project.url }}" target="_blank">Apri in GitHub</a></td>
+        <td class="align-middle"></td>
+    </tr>
+</template>
+ 
+<style scoped></style>
