50a42a2e0ef371dc853d7788d6a40c6fb6731aec
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-12 14:48:55 +0200

    Add Page Not Found

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'NotFoundPage',
+};
+</script>
+
+<template>
+  <div class="vh-100 bg-dark flex-column  d-flex align-items-center  justify-content-center ">
+    <h3 class="text-white">404 | Page not found</h3>
+    <button class="btn-btn-secondary" @click="$router.push({ name: 'home' })">Torna alla home</button>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 // importo le pagine
 import HomePage from '../pages/HomePage.vue';
 import ProjectPage from '../pages/ProjectPage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
+  linkExactActiveClass: 'active',
   routes: [
     { path: '/', name: 'home', component: HomePage },
-    { path: '/project', name: 'home', component: ProjectPage },
+    { path: '/project', name: 'project', component: ProjectPage },
+    { path: '/:pathMatch(.*)*', component: NotFoundPage },
   ],
 });
 
